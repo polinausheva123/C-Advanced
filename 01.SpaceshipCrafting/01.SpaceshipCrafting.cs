0e98715e7c8f2fe5d977e71fbc237513f65c0fe6
polinausheva123 <53872655+polinausheva123@users.noreply.github.com>
2019-08-07 23:43:39 +0300

    Add files via upload

---
+++ 01.SpaceshipCrafting/01.SpaceshipCrafting.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SpaceshipCrafting
+{
+    public class SpaceshipCrafting
+    {
+        private static void Main()
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            Stack<int> items = new Stack<int>(Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            var data = new Dictionary<int, string>()
+            {
+                {50, "Aluminium" },
+                {75, "Lithium" },
+                {100, "Carbon fiber" },
+                {25, "Glass"}
+            };
+
+            List<string> madeItems = new List<string>();
+
+            while (liquids.Count != 0 && items.Count != 0)
+            {
+                int currentChemicalLiquid = liquids.Dequeue();
+                int currentPhysicalItem = items.Pop();
+
+                int sum = currentChemicalLiquid + currentPhysicalItem;
+
+                if (data.ContainsKey(sum))
+                {
+                    madeItems.Add(data[sum]);
+                }
+                else
+                {
+                    items.Push(currentPhysicalItem + 3);
+                }
+            }
+
+            if (madeItems.Contains("Lithium") &&
+                madeItems.Contains("Carbon fiber") &&
+                madeItems.Contains("Glass") &&
+                madeItems.Contains("Aluminium"))
+            {
+                Console.WriteLine("Wohoo! You succeeded in building the spaceship!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to build the spaceship.");
+            }
+
+            if (liquids.Count != 0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (items.Count != 0)
+            {
+                Console.WriteLine($"Physical items left: {string.Join(", ", items)}");
+            }
+            else
+            {
+                Console.WriteLine("Physical items left: none");
+            }
+
+            Console.WriteLine($"Aluminium: {madeItems.Count(x => x == "Aluminium")}");
+            Console.WriteLine($"Carbon fiber: {madeItems.Count(x => x == "Carbon fiber")}");
+            Console.WriteLine($"Glass: {madeItems.Count(x => x == "Glass")}");
+            Console.WriteLine($"Lithium: {madeItems.Count(x => x == "Lithium")}");
+        }
+    }
+}
---
+++ 02.SpaceStationRecruitment/02. SpaceStationRecruitment/SpaceStation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+        //collection 
+
+        private List<Astronaut> astronauts;
+
+        public int Count
+          => this.astronauts.Count;
+        public SpaceStation(string listName, int capacity)
+        {
+            this.Capacity = capacity;
+            this.Name = listName;
+
+
+            astronauts = new List<Astronaut>(capacity);
+            
+
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public void Add(Astronaut someAustronaut)
+        {         
+            //add if there is space
+            
+           if(astronauts.Count< this.Capacity)
+            {
+                astronauts.Add(someAustronaut);
+                
+            }
+        }
+
+        public bool Remove(string name)
+        {
+
+            for (int i = 0; i < astronauts.Count; i++)
+            {
+
+                if (astronauts[i].Name == name)
+                {
+                    this.astronauts.RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            var austrPerson = this.astronauts.OrderByDescending(a => a.Age).FirstOrDefault();
+            return austrPerson;
+
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            var austr = this.astronauts.FirstOrDefault(x => x.Name == name);
+
+            return austr;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb2 = new StringBuilder();
+
+            sb2.AppendLine($"Astronauts working at Space Station {this.Name}:");
+
+            foreach (var item in astronauts)
+            {
+                sb2.AppendLine(item.ToString());
+            }
+            //"Astronaut: {name}, {age} ({country})"
+            return sb2.ToString().TrimEnd();
+
+            //Astronaut: Stephen, 40 (Bulgaria)
+            //Astronaut: Mark, 34 (UK)
+        }
+    }
+}
